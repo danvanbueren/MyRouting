f2fe11ac1f64212cf95b1d11b06a2e4a1d758e59
Daniel Van Bueren <danvb10@gmail.com>
2024-01-28 02:40:38 +0100

    Boilerplate

---
+++ src/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en" data-bs-core="modern" data-bs-theme="dark">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MyRouting</title>
+
+    <link href="css/halfmoon/halfmoon.min.css" rel="stylesheet">
+    <link href="css/halfmoon/cores/halfmoon.cores.css" rel="stylesheet">
+    <link href="css/google/materialSymbols.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg" style="background-color: var(--bs-content-bg); border-bottom: var(--bs-border-width) solid var(--bs-content-border-color);">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <span class="material-symbols-outlined">history_edu</span>
+            <span class="ps-2">MyRouting</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+            </ul>
+            <button class="btn btn-primary"><span class="d-flex align-items-center"><span class="material-symbols-outlined fs-6">login</span><span class="ps-1">Log In</span></span></button>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row py-1">
+        <div class="col">Column</div>
+    </div>
+</div>
+
+<script src="js/bootstrap/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
